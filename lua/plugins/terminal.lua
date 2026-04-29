ee4660ab6fd4db9d141da5a22f7a8b676e098eae
ReeF <vesecky.lukas90@gmail.com>
2026-04-30 01:45:59 +0200

    added runner terminal

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,27 @@
+return {
+  'TheLazyCat00/runner-nvim',
+  opts = {},
+  keys = {
+    {
+      '<leader>r',
+      function()
+        require('runner-nvim').runLast()
+      end,
+      desc = 'Run last cmd',
+    },
+    {
+      '<leader>o',
+      function()
+        require('runner-nvim').run()
+      end,
+      desc = 'Run cmd',
+    },
+    {
+      '<leader>tt',
+      function()
+        require('runner-nvim').toggle()
+      end,
+      desc = 'Toggle Terminal',
+    },
+  },
+}
