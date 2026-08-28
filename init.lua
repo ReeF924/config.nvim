ede069d82923f3243753140f61a702e1c3f089ea
ReeF <vesecky.lukas90@gmail.com>
2026-08-28 17:32:53 +0200

    swapped <leader>sf and <leader>sF

--- init.lua
+++ init.lua
@@ -372,11 +372,11 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>sF', function()
+      vim.keymap.set('n', '<leader>sf', function()
         builtin.find_files {
           find_command = { 'fd', '--type', 'f', '--hidden', '--exclude', '.git', '--no-ignore' },
         }
+      vim.keymap.set('n', '<leader>sF', builtin.find_files, { desc = '[S]earch [F]iles' })
       end, { desc = '[S]earch [F]iles (including hidden)' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
